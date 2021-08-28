76ba0dd76329cf458e13c17f66614dfa95a0e796
Yzuriha <inoriislove@gmail.com>
2021-08-28 19:04:25 +0200

    Clean project

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="home">
-    <Sheet/>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Sheet from '@/components/Sheet.vue'
+import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    Sheet
+    HelloWorld
   }
 }
 </script>
