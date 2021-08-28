e4b664c470ca42555361fd54a44a0944f7e16fbd
Yzuriha <inoriislove@gmail.com>
2021-08-28 15:08:39 +0200

    Update instagram-rss.yml

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
-        <router-link class="navbar-brand col-sm-3 col-md-2 mr-0" to="/">VueSheets</router-link>
+        <router-link class="navbar-brand col-sm-3 col-md-2 mr-0" to="/">VueSheet</router-link>
         <!-- <input class="form-control form-control-dark w-100" type="text" placeholder="Search" aria-label="Search"> -->
       </nav>
     </div>
