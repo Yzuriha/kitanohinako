d8c29c0e7d7d7ca33ad537137d8397474bd4b72d
Yzuriha <inoriislove@gmail.com>
2021-09-13 00:02:58 +0200

    Split chunks

--- vue.config.js
+++ vue.config.js
@@ -15,24 +15,24 @@ module.exports = {
                 minRatio: 0.8,
             }
         }
+    },
+    configureWebpack: {
+        optimization: {
+            runtimeChunk: 'single',
+            splitChunks: {
+                chunks: 'all',
+                maxInitialRequests: Infinity,
+                minSize: 0,
+                cacheGroups: {
+                    vendor: {
+                        test: /[\\/]node_modules[\\/]/,
+                        name(module) {
+                            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+                            return `npm.${packageName.replace('@', '')}`;
+                        },
+                    },
+                },
+            },
+        },
     }
-    // configureWebpack: {
-    //     optimization: {
-    //         runtimeChunk: 'single',
-    //         splitChunks: {
-    //             chunks: 'all',
-    //             maxInitialRequests: Infinity,
-    //             minSize: 0,
-    //             cacheGroups: {
-    //                 vendor: {
-    //                     test: /[\\/]node_modules[\\/]/,
-    //                     name(module) {
-    //                         const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-    //                         return `npm.${packageName.replace('@', '')}`;
-    //                     },
-    //                 },
-    //             },
-    //         },
-    //     },
-    // }
 }
