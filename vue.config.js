10e18404937baea12d9a9725e2fe109b003dfc92
Yzuriha <inoriislove@gmail.com>
2021-09-12 23:22:39 +0200

    Update Vue Config

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/kitanohinako/'
-        : '/',
+    publicPath: '/',
     pwa: {
         workboxOptions: {
             skipWaiting: true,
