dc3428f76c6b69a3d75e2796de7793e658650579
Yzuriha <inoriislove@gmail.com>
2021-09-18 00:59:57 +0200

    Add more works

--- src/views/works.vue
+++ src/views/works.vue
@@ -3,10 +3,6 @@
     <base-section title="WORKS" additional-classes="headline--center" class="">
       <template v-slot:content>
         <work-card v-for="(work, index) in worksData" :title="work.title" :works="work.data" :animation-delay-number="index"></work-card>
-<!--        <work-card title="PHOTO BOOKS" :works="photobooks"></work-card>-->
-<!--        <work-card title="WEB" :works="web"></work-card>-->
-<!--        <work-card title="DRAMA" :works="drama"></work-card>-->
-<!--        <work-card title="STAGE PLAY" :works="stagePlay"></work-card>-->
       </template>
     </base-section>
   </div>
@@ -23,12 +19,67 @@ export default {
     return {
       worksData: [
         {
-          title: "PHOTO BOOKS",
+          title: "PHOTO BOOK",
           data: [
             {
               kanji: '空気の色',
               romaji: 'Kuuki no Iro',
               year: '2020'
+            },
+          ],
+        },
+        {
+          title: "MAGA- ZINE",
+          data: [
+            {
+              kanji: '遠回りの"希望" (UTB vol.305)',
+              romaji: 'Toumawari no Kibou',
+              year: '2021'
+            },
+            {
+              kanji: 'Sailing Day (EX大衆　5月号)',
+              romaji: 'Sailing Day',
+              year: '05.2021'
+            },
+            {
+              kanji: '君と過ごしたあの時間いつまでも忘れない (B.L.T.　8月号)',
+              romaji: 'Kimi to Sugoshita Ano Jikan Itsumademo Wasurenai',
+              year: '08.2021'
+            },
+            {
+              kanji: "It's my place (blt graph.vol.61)",
+              romaji: 'It\'s my place',
+              year: '2021'
+            },
+            {
+              kanji: '君をみつけたあの日から (B.L.T.　12月号)',
+              romaji: 'Kimi wo Mitsuketa Ano Hi',
+              year: '12.2019'
+            },
+            {
+              kanji: '転校生のトゲ (WBP #8)',
+              romaji: 'Tenkousei Toge',
+              year: '2016'
+            },
+            {
+              kanji: 'キミが笑顔でいてくれるなら (FLASHスペシャル グラビアBEST秋号 2015年11月号)',
+              romaji: 'Kimi ga Egao de Ite Kurerunara',
+              year: '11.2015'
+            },
+            {
+              kanji: 'やるなら今しかねえ (BUBKA vol.45)',
+              romaji: 'Yaru nara Imashikanee',
+              year: '2015'
+            },
+            {
+              kanji: 'キタノスポーツ (UTB vol.233)',
+              romaji: 'Kitano Sports',
+              year: '2015'
+            },
+            {
+              kanji: 'ぼく勉強ができない (EX大衆　9月号)',
+              romaji: 'Boku ga Denkyou dekinai',
+              year: '09.2015'
             },
           ],
         },
