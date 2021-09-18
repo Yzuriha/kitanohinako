1da1708e0a486b9bd2fe571389e026e0b06cd59e
Yzuriha <inoriislove@gmail.com>
2021-09-18 17:21:47 +0200

    Change Filter text

--- src/components/input/input-text.vue
+++ src/components/input/input-text.vue
@@ -2,12 +2,12 @@
   <div class="input_group">
     <input type="text"
            class="input_field"
-           placeholder="Filter Blogs"
+           placeholder="Find Blog by Date"
            name="name" id='name'
            spellcheck="false"
            :value="modelValue"
            @input="$emit('update:modelValue', $event.target.value)"/>
-    <label for="name" class="input_label">Filter Blogs</label>
+    <label for="name" class="input_label">Find Blog by Date</label>
   </div>
 </template>
 
